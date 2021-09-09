3599325209e28ecc02c6bae1a8885ee6ac6ba058
pifragile <pigu@bluewin.ch>
2021-09-09 11:01:42 +0200

    update analyze_logs to count newbies

--- analyze_logs.py
+++ analyze_logs.py
@@ -2,6 +2,7 @@ import os
 with open('skips_prime_below.csv', 'w') as skip_f:
     skip_f.write('num_skips\n')
     with open('lens_prime_below.csv', 'w') as len_f:
+        len_f.write('len_meetups,num_newbies\n')
         num_verified = 0
         num_meetups = 0
         for filename in os.listdir('.'):
@@ -10,8 +11,15 @@ with open('skips_prime_below.csv', 'w') as skip_f:
                 with open(filename, 'r') as f:
                     for l in f:
                         if l.startswith('['):
-                            len_meetup = len(l.split(','))
-                            len_f.write(f'{len_meetup}\n')
+                            len_meetup = 0
+                            num_newbies = 0
+                            for c in l:
+                                if c in ['B', 'R', 'E']:
+                                    len_meetup += 1
+                                elif c == 'N':
+                                    len_meetup += 1
+                                    num_newbies += 1
+                            len_f.write(f'{len_meetup},{num_newbies}\n')
                         else:
                             l = l.strip()
                             s = 'Skipped primes X times: '
