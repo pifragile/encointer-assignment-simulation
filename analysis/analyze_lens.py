2a87b75479f50806f98c9ad957f3ecc810eea9f0
pifragile <pigu@bluewin.ch>
2021-09-09 16:00:07 +0200

    add code for analyzing newbie ratios

--- analysis/analyze_lens.py
+++ analysis/analyze_lens.py
@@ -1,23 +1,39 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import csv
 
 # use this to preprocess the data, as distplot is way too slow for our huge dataset
-pre_process = False
+pre_process = True
 if pre_process:
     with open('data/lens_prime_below.csv', 'r') as f:
-        counter = {}
+        length_counter = {}
+        newbie_ratio_counter = {}
+        f.readline()
         for l in f:
-            l = int(l)
-            if l in counter.keys():
-                counter[l] += 1
+            length, num_newbies = l.split(',')
+            length = int(length)
+            num_newbies = int(num_newbies)
+            newbie_ratio = round(num_newbies / length, 2)
+            if length in length_counter.keys():
+                length_counter[length] += 1
             else:
-                counter[l] = 1
+                length_counter[length] = 1
 
-    print(counter)
-    lens = counter
+            if newbie_ratio in newbie_ratio_counter.keys():
+                newbie_ratio_counter[newbie_ratio] += 1
+            else:
+                newbie_ratio_counter[newbie_ratio] = 1
+
+    print('length count:')
+    print(length_counter)
+    print('newbie ratio count:')
+    print(newbie_ratio_counter)
+    lens = length_counter
+    newbie_ratios = newbie_ratio_counter
 else:
     lens = {3: 1884, 4: 17977, 11: 849570493, 10: 656302972, 9: 1354037135, 8: 30858292, 5: 6470, 7: 29127, 6: 17907,
             12: 258253969, 13: 10003897, 14: 132896, 15: 627}
+    newbie_ratios = {}
 
 df = pd.DataFrame(lens.items(), columns=['num_participants', 'amount_meetups'])
 
@@ -32,3 +48,19 @@ plt.title('Meetup size distribution')
 plt.xlabel('Number of participants')
 plt.ylabel('Number of meetups')
 plt.savefig('plots/meetup_size_distribution.png')
+
+
+df = pd.DataFrame(newbie_ratios.items(), columns=['newbie_ratio', 'amount_meetups'])
+
+print(f"""
+Sum of meetups: {df['amount_meetups'].sum()}
+""")
+
+df = df.sort_values('newbie_ratio')
+ax = df.plot.bar(x='newbie_ratio', y='amount_meetups', rot=0)
+ax.get_legend().remove()
+plt.yscale('log')
+plt.title('Newbie ratio distribution')
+plt.xlabel('Newbie ratio')
+plt.ylabel('Number of meetups')
+plt.savefig('plots/newbie_ratio_distribution.png')
